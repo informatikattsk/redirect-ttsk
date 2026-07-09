ddf927a1a6d4b8e683813cd5a42e6c872711bf29
Andrej Srna <andrejsrna@users.noreply.github.com>
2026-07-09 09:42:50 +0000

    Serve static export with nginx

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,12 @@ import type { NextConfig } from "next";
 import path from "path";
 
 const nextConfig: NextConfig = {
+  output: "export",
   turbopack: {
     root: path.resolve(),
+  },
+  images: {
+    unoptimized: true,
   },
 };
 
